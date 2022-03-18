11df3adb1e8df2cbc55899b0ff539bdb1ed68dc5
Alice Anglesjö <alice.anglesjo@hotmail.com>
2022-03-18 14:26:29 +0100

    login on mobile

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -161,27 +161,19 @@ export const NavBar = () => {
               >
                 Dashboard
               </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-              >
-                Team
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-              >
-                Projects
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-              >
-                Calendar
-              </Disclosure.Button>
+              {!session && (
+                <Disclosure.Button
+                  as="a"
+                  href={`/api/auth/signin`}
+                  onClick={(e: { preventDefault: () => void }) => {
+                    e.preventDefault()
+                    signIn()
+                  }}
+                  className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                >
+                  Sign in
+                </Disclosure.Button>
+              )}
             </div>
             {session && session.user && (
               <div className="pt-4 pb-3 border-t border-gray-200">
