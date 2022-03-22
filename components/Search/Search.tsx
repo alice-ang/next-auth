652be47907e029a628ebb60b36dc45390498a40b
Alice Anglesjö <alice.anglesjo@hotmail.com>
2022-03-23 00:05:53 +0100

    search icon

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import { SearchIcon, AcademicCapIcon } from "@heroicons/react/solid"
+import { AcademicCapIcon } from "@heroicons/react/solid"
 
 export const Search = () => {
   return (
@@ -23,7 +23,6 @@ export const Search = () => {
           type="button"
           className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
         >
-          <SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
           <span>Search</span>
         </button>
       </div>
