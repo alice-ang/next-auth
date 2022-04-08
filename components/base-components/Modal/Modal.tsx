5a632db9f04832947712e4acfd9187ae0be27cc1
Alice Anglesjö <alice.anglesjo@hotmail.com>
2022-04-08 16:11:05 +0200

    change modal of not logged in

--- components/base-components/Modal/Modal.tsx
+++ components/base-components/Modal/Modal.tsx
@@ -1,8 +1,9 @@
 import { FC, Fragment, useRef } from "react"
 import { Dialog, Transition } from "@headlessui/react"
-import { PencilIcon } from "@heroicons/react/solid"
+import { PencilIcon, EmojiSadIcon } from "@heroicons/react/solid"
 import { Range } from "../Inputs"
 import { addReview, useAuth } from "../../../utils"
+import { useRouter } from "next/router"
 
 type ModalProps = {
   closeModal: any
@@ -10,6 +11,7 @@ type ModalProps = {
 
 export const Modal: FC<ModalProps> = ({ closeModal }) => {
   const { user } = useAuth()
+  const router = useRouter()
   const cancelButtonRef = useRef(null)
 
   const kitchenRef = useRef<HTMLInputElement>(null)
@@ -62,119 +64,155 @@ export const Modal: FC<ModalProps> = ({ closeModal }) => {
           >
             <div className="relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
               <div>
-                <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-indigo-100">
-                  <PencilIcon
-                    className="h-6 w-6 text-indigo-600"
-                    aria-hidden="true"
-                  />
-                </div>
-                <div className="mt-3 text-center sm:mt-5">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg leading-6 font-medium text-gray-900"
-                  >
-                    Write a review
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <Range title="kitchen" ref={kitchenRef} />
-                    <Range title="bathroom" ref={bathroomRef} />
-                    <Range title="washroom" ref={washroomRef} />
-                    <Range title="internet" ref={internetRef} />
-
-                    <div className="border-t border-gray-200 mt-5 pt-5" />
-
-                    <div className="mt-3">
-                      <label
-                        htmlFor="review"
-                        className="text-left block text-m font-medium text-gray-700"
-                      >
-                        Add your feedback
-                      </label>
-                      <div className="mt-2">
-                        <textarea
-                          ref={feedbackRef}
-                          rows={4}
-                          name="review"
-                          id="review"
-                          placeholder="I lived here during..."
-                          className=" p-3 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm  border border-gray-300 rounded-md"
-                          defaultValue={""}
-                        />
-                      </div>
+                {user ? (
+                  <>
+                    <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-indigo-100">
+                      <PencilIcon
+                        className="h-6 w-6 text-indigo-600"
+                        aria-hidden="true"
+                      />
                     </div>
-
-                    <div className="mt-3 ">
-                      <label
-                        htmlFor="review"
-                        className="text-left block text-m font-medium text-gray-700"
+                    <div className="mt-3 text-center sm:mt-5">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg leading-6 font-medium text-gray-900"
                       >
-                        Add photos
-                      </label>
-                      <div className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md mt-2">
-                        <div className="space-y-1 text-center">
-                          <svg
-                            className="mx-auto h-12 w-12 text-gray-400"
-                            stroke="currentColor"
-                            fill="none"
-                            viewBox="0 0 48 48"
-                            aria-hidden="true"
+                        Write a review
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <Range title="kitchen" ref={kitchenRef} />
+                        <Range title="bathroom" ref={bathroomRef} />
+                        <Range title="washroom" ref={washroomRef} />
+                        <Range title="internet" ref={internetRef} />
+
+                        <div className="border-t border-gray-200 mt-5 pt-5" />
+
+                        <div className="mt-3">
+                          <label
+                            htmlFor="review"
+                            className="text-left block text-m font-medium text-gray-700"
                           >
-                            <path
-                              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                              strokeWidth={2}
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
+                            Add your feedback
+                          </label>
+                          <div className="mt-2">
+                            <textarea
+                              ref={feedbackRef}
+                              rows={4}
+                              name="review"
+                              id="review"
+                              placeholder="I lived here during..."
+                              className=" p-3 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm  border border-gray-300 rounded-md"
+                              defaultValue={""}
                             />
-                          </svg>
-                          <div className="flex text-sm text-gray-600">
-                            <label
-                              htmlFor="file-upload"
-                              className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                            >
-                              <span>Upload a file</span>
-                              <input
-                                id="file-upload"
-                                name="file-upload"
-                                type="file"
-                                className="sr-only"
-                              />
-                            </label>
-                            <p className="pl-1">or drag and drop</p>
                           </div>
-                          <p className="text-xs text-gray-500">
-                            PNG, JPG, GIF up to 10MB
-                          </p>
+                        </div>
+
+                        <div className="mt-3 ">
+                          <label
+                            htmlFor="review"
+                            className="text-left block text-m font-medium text-gray-700"
+                          >
+                            Add photos
+                          </label>
+                          <div className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md mt-2">
+                            <div className="space-y-1 text-center">
+                              <svg
+                                className="mx-auto h-12 w-12 text-gray-400"
+                                stroke="currentColor"
+                                fill="none"
+                                viewBox="0 0 48 48"
+                                aria-hidden="true"
+                              >
+                                <path
+                                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                  strokeWidth={2}
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                              </svg>
+                              <div className="flex text-sm text-gray-600">
+                                <label
+                                  htmlFor="file-upload"
+                                  className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                                >
+                                  <span>Upload a file</span>
+                                  <input
+                                    id="file-upload"
+                                    name="file-upload"
+                                    type="file"
+                                    className="sr-only"
+                                  />
+                                </label>
+                                <p className="pl-1">or drag and drop</p>
+                              </div>
+                              <p className="text-xs text-gray-500">
+                                PNG, JPG, GIF up to 10MB
+                              </p>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-indigo-100">
+                      <EmojiSadIcon
+                        className="h-6 w-6 text-indigo-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-5">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg leading-6 font-medium text-gray-900"
+                      >
+                        You must be logged in to write a review
+                      </Dialog.Title>
+                    </div>
+                  </>
+                )}
               </div>
               <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
-                <button
-                  type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
-                  onClick={async () => {
-                    if (
-                      kitchenRef.current &&
-                      bathroomRef.current &&
-                      washroomRef.current &&
-                      internetRef.current &&
-                      feedbackRef.current
-                    ) {
-                      await addReview({
-                        kitchen: kitchenRef.current.value,
-                        bathroom: bathroomRef.current.value,
-                        washroom: washroomRef.current.value,
-                        internet: internetRef.current.value,
-                        feedback: feedbackRef.current.value,
-                      }).then(() => handleCancelClick)
-                    }
-                    handleCancelClick
-                  }}
-                >
-                  Post
-                </button>
+                {user ? (
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+                    onClick={async () => {
+                      if (
+                        kitchenRef.current &&
+                        bathroomRef.current &&
+                        washroomRef.current &&
+                        internetRef.current &&
+                        feedbackRef.current
+                      ) {
+                        await addReview({
+                          kitchen: kitchenRef.current.value,
+                          bathroom: bathroomRef.current.value,
+                          washroom: washroomRef.current.value,
+                          internet: internetRef.current.value,
+                          feedback: feedbackRef.current.value,
+                        }).then(() => handleCancelClick)
+                      } else {
+                        handleCancelClick
+                      }
+                    }}
+                  >
+                    Post
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+                    onClick={() => {
+                      router.push("/login")
+                      handleCancelClick
+                    }}
+                  >
+                    Login
+                  </button>
+                )}
+
                 <button
                   type="button"
                   className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
