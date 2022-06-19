4bbfe03a7bc0fe5b93f1e4144179af4a464d1d24
Alice A <theblackhairday@hotmail.com>
2022-06-20 00:43:10 +0200

    tweaks

--- components/base-components/HorizontalScroll/HorizontalScroll.tsx
+++ components/base-components/HorizontalScroll/HorizontalScroll.tsx
@@ -2,17 +2,26 @@ import { FC } from "react"
 
 type ScrollProps = {
   children: any
+  isLarge?: boolean
 }
 
-export const HorizontalScroll: FC<ScrollProps> = ({ children }) => {
+export const HorizontalScroll: FC<ScrollProps> = ({ children, isLarge }) => {
   return (
     <div className="flex flex-no-wrap overflow-x-scroll scrolling-touch items-start mb-8">
       {children &&
         children.map((child: any) => {
           return (
-            <div className="flex-none w-2/3 md:w-1/4 lg:1/3 mr-8 md:pb-4 ">
-              {child}
-            </div>
+            <>
+              {isLarge ? (
+                <div className="flex-none w-2/3 md:w-1/3 lg:1/3 mr-8 md:pb-4 ">
+                  {child}
+                </div>
+              ) : (
+                <div className="flex-none w-2/3 md:w-1/4 lg:1/3 mr-8 md:pb-4 ">
+                  {child}
+                </div>
+              )}
+            </>
           )
         })}
     </div>
--- pages/discounts.tsx
+++ pages/discounts.tsx
@@ -6,13 +6,13 @@ export default function DiscountPage() {
       <Banner>
         <h2>hej</h2>
       </Banner>
-      <div className="m-auto w-full md:w-[80%]">
+      <div className="m-auto w-full px-4 md:px-0 md:w-[80%]">
         <h2 className="text-3xl py-4">Rabatter</h2>
         <h3 className="text-2xl">Horizontal list</h3>
         <div className="py-8">
-          <HorizontalScroll>
+          <HorizontalScroll isLarge>
             {[0, 1, 2, 3, 4].map(() => {
-              return <AdCard hideButton />
+              return <AdCard />
             })}
           </HorizontalScroll>
         </div>
